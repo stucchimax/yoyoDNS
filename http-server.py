cf874432b70c254911b5d4356905b405253e166d
Massimiliano Stucchi <max@stucchi.ch>
2024-08-09 11:37:34 +0200

    Add new http server, based on FastAPI.
    The http server has a specific endpoint to be used to query for
    information about a particular UUID request in a particular DB.
    
    This is in particular used in conjunction with requests for the
    measurements together with NAMEX, going out via brightdata and hence not
    using the RIPE Atlas system.

---
+++ http-server.py
@@ -0,0 +1,29 @@
+
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.get("/")
+def main(request: Request):
+    return("Hello World")
+
+@app.get('/get_referer')
+def referer(request: Request):
+    address = request.headers.get('referer')
+    return address
+
+@app.get['/uuid/{dbase}/{uuid}']
+def uuid(request: Requesti, dbase, uuid):
+    
+    query = f"SELECT * from queries WHERE uuid=\"{uuid}\""
+
+    con = sqlite3.connect(f'{dbase}.sql', check_same_thread=False)
+    res = con.execute(query)
+
+    entries = res.fetchall()
+
+    con.close()
+
+    return entries
+
