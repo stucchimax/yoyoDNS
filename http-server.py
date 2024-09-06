76666bcc57d858a078626fd40bb5434dac475741
Massimiliano Stucchi <max@stucchi.ch>
2024-09-06 14:55:44 +0200

    Add endpoint to create the database, and check for its existence in the original endpoint.

--- http-server.py
+++ http-server.py
@@ -1,29 +1,53 @@
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
+
+import os
+import sqlite3
 
 app = FastAPI()
 
-
 @app.get("/")
-def main(request: Request):
+async def main(request: Request):
     return("Hello World")
 
 @app.get('/get_referer')
-def referer(request: Request):
+async def referer(request: Request):
     address = request.headers.get('referer')
     return address
 
 @app.get['/uuid/{dbase}/{uuid}']
-def uuid(request: Request, dbase, uuid):
+async def uuid(request: Request, dbase, uuid):
     
     query = f"SELECT * from queries WHERE uuid=\"{uuid}\""
 
-    con = sqlite3.connect(f'{dbase}.sql', check_same_thread=False)
-    res = con.execute(query)
+    if os.path.isfile(f"{dbase}.sql")
+        con = sqlite3.connect(f'{dbase}.sql', check_same_thread=False)
+        res = con.execute(query)
 
-    entries = res.fetchall()
+        entries = res.fetchall()
+
+        con.close()
+
+        return entries
+    else:
+        raise HTTPException(status_code=404, detail="Database not found") 
+
+@app.get['/dbase/{dbase}']
+async def dbase_create(request: Request, dbase):
+
+    con = sqlite3.connect(f'{dbase}.sql', check_same_thread=False)
+
+    query = "CREATE TABLE queries (uuid VARCHAR, ip_address VARCHAR, query_timestamp INTEGER, query_asn INTEGER, query_as_name VARCHAR, query_location VARCHAR)"
+
+    con.execute(query)
+
+    con.commit()
 
     con.close()
 
-    return entries
+    return("Database created")
+
+
+
+    
 
